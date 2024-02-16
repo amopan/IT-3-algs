f7ad730c960dcbfed63d76c97a5177f38270a72c
Alexander.Mopan <alexander.mopan@tasso.net>
2024-02-16 11:22:03 +0200

    left some comment

--- Demo.App/Program.cs
+++ Demo.App/Program.cs
@@ -3,6 +3,7 @@
 Console.WriteLine("Please type in some number");
 var nStr = Console.ReadLine();
 
+//simple stupid code, no test required
 if (!int.TryParse(nStr, out n)){
     Console.WriteLine("It is not a number!");
 }
